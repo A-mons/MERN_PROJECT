6b4abff5a801ad40c633623a2715421a57018aca
Mons <ah.mounsef@gmail.com>
2021-03-21 05:11:55 +0100

    schema model creation/create Posts/getting Posts

---
+++ server/models/postMessage.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+////////////////Creating Post Schema Model////////////////
+
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+});
+
+const postMessage = mongoose.model('postMessage', postSchema);
+export default postMessage;
