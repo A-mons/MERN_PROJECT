48f4ed06faa09f67de6b461b3afc632fe14acd5a
Mons <ah.mounsef@gmail.com>
2021-04-16 19:03:24 +0200

    creating component"create posts  form"

--- server/index.js
+++ server/index.js
@@ -10,14 +10,14 @@ const app = express();
 
 dotenv.config();
 
-app.use('/posts', postRoutes);
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(` server is running on Port: ${ PORT } ` )))
